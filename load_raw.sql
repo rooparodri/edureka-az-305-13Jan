15f869e3c52a340e744c9be1c62d25459e80c4f4
rooparodri <139795316+rooparodri@users.noreply.github.com>
2026-04-06 01:12:53 -0500

    Create SQL script for salary data loading and curation
    This SQL script creates a pipeline for loading salary data, including staging, validation, and final curation of the data into a curated table.

---
+++ load_raw.sql
@@ -0,0 +1,82 @@
+--File → Stage → COPY INTO → Raw Table → Validation → SCD2 Merge → Fact Table
+USE DATABASE db_fidelity;
+
+CREATE OR REPLACE FILE FORMAT ff_salary
+  TYPE = 'CSV'
+  FIELD_DELIMITER = '|'
+  SKIP_HEADER = 0
+  NULL_IF = ('NULL', '');
+
+CREATE OR REPLACE STAGE stg_salary
+  FILE_FORMAT = ff_salary;
+
+CREATE OR REPLACE TABLE raw_salary (
+    EMP_ID STRING,
+    ROLE STRING,
+    SALARY STRING
+);
+
+
+USE DATABASE DB_FIDELITY;
+COPY INTO raw_salary
+FROM @stg_salary/tb_emp_salary
+ON_ERROR = 'CONTINUE';
+
+-- ⭐ Capture the COPY job ID
+SET job_id = LAST_QUERY_ID();
+
+-- Check COPY history (optional)
+SELECT *
+FROM TABLE(
+    INFORMATION_SCHEMA.COPY_HISTORY(
+        TABLE_NAME => 'RAW_SALARY',
+        START_TIME => DATEADD('hour', -24, CURRENT_TIMESTAMP())
+    )
+);
+
+-- ⭐ Correct VALIDATE usage
+CREATE OR REPLACE TABLE load_errors AS
+SELECT
+    CURRENT_TIMESTAMP() AS LOAD_TIME,
+    REJECTED_RECORD AS RAW_ROW,
+    ERROR AS ERROR_MESS.AGE
+FROM TABLE(
+    VALIDATE(
+        RAW_SALARY,
+        JOB_ID => $job_id
+    )
+);
+
+CREATE OR REPLACE TABLE stg_salary_clean AS
+SELECT
+    EMP_ID,
+    TRIM(ROLE) AS ROLE,
+    TRY_TO_NUMBER(SALARY) AS SALARY
+FROM raw_salary;
+
+--remove nulls
+CREATE OR REPLACE TABLE stg_salary_valid AS
+SELECT *
+FROM stg_salary_clean
+WHERE EMP_ID IS NOT NULL
+  AND ROLE IS NOT NULL
+  AND SALARY IS NOT NULL;
+
+--add datetime
+  CREATE OR REPLACE TABLE curated_salary (
+    EMP_ID STRING,
+    ROLE STRING,
+    SALARY NUMBER(10,2),
+    LOAD_DATE DATE
+);
+
+INSERT INTO curated_salary
+SELECT
+    EMP_ID,
+    ROLE,
+    SALARY,
+    CURRENT_DATE() AS LOAD_DATE
+FROM stg_salary_valid;
+
+SELECT * FROM curated_salary;
+0SELECT * FROM stg_salary_valid;
